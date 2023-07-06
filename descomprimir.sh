e331ba2340a5bbf62179e86e6d22e7ab71a15daa
Pilafis Franco <pilafisf@gmail.com>
2023-07-05 22:49:26 -0300

    Archivo descomprimir.sh terminado sin el segundo argumento de verificacion

--- descomprimir.sh
+++ descomprimir.sh
@@ -5,21 +5,28 @@ con las imágenes comprimidas y otro con una suma de verificación). Si
 ocurrió algún error se debe informar al usuario de lo contrario se procede
 a descomprimir"""
 
-existe_test=./test.txt
-
-test1=$(cat test.txt | sed -n 2p | cut -c 1-32 )
-test2=$(cat test.txt | sed -n 4p | cut -c 1-32 )
-
-echo $test1
-echo $test2
-
-if [ $test1 == $test2 ] && [ -f $existe_test ]
-then
-    unzip archivo.zip
-else
-    echo "Error: falta comprobacion de archvivos"
+# Verificar que se paso un argumento
+if [ "$#" -ne 1 ]; then
+        echo "ERROR: Se requiere un argumento"
+        exit 1
 fi
 
-#cat test.txt 
+# Obtener los nombres de los archivos de entrada
+archivo_comprimido=$1
 
+
+# Verificar que existe el archivo
+if [ ! -f "$archivo_comprimido" ]; then
+        echo "Error: el archivo comprimido '$archivo_comprimido' no existe."
+        exit 2
+fi
+
+
+mkdir -p imagenes_descomprimidas
+
+unzip "$archivo_comprimido" -d imagenes_descomprimidas
+
+echo "Se descomprimio correctamente"
+
+exit 0
 
